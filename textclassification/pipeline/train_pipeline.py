bf11a32532f23e40a241dde00b1c3e79c2b1f363
pranaynaik23 <pranaynaik007@hotmail.com>
2024-07-13 21:56:21 +0530

    added model_evaluation and model pusher config

--- textclassification/pipeline/train_pipeline.py
+++ textclassification/pipeline/train_pipeline.py
@@ -5,9 +5,11 @@ from textclassification.components.data_ingestion import DataIngestion
 from textclassification.components.data_validation import DataValidation
 from textclassification.components.data_transformation import DataTransformation
 from textclassification.components.model_trainer import ModelTrainer
-from textclassification.entity.config_entity import DataIngestionConfig,DataValidationConfig,DataTransformationConfig,ModelTrainerConfig
+from textclassification.components.model_evaluation import ModelEvaluation
+from textclassification.components.model_pusher import ModelPusher
+from textclassification.entity.config_entity import DataIngestionConfig,DataValidationConfig,DataTransformationConfig,ModelTrainerConfig,ModelEvaluationConfig,ModelPusherConfig
 
-from textclassification.entity.artifact_entity import DataIngestionArtifacts,DataValidationArtifacts,DataTransformationArtifacts,ModelTrainerArtifacts
+from textclassification.entity.artifact_entity import DataIngestionArtifacts,DataValidationArtifacts,DataTransformationArtifacts,ModelTrainerArtifacts,ModelEvaluationArtifacts,ModelPusherArtifacts
 
 
 class TrainPipeline:
@@ -16,6 +18,8 @@ class TrainPipeline:
         self.data_validation_config = DataValidationConfig()
         self.data_transformation_config = DataTransformationConfig()
         self.model_trainer_config = ModelTrainerConfig()
+        self.model_evaluation_config = ModelEvaluationConfig()
+        self.model_pusher_config = ModelPusherConfig()
 
 
     
@@ -84,6 +88,36 @@ class TrainPipeline:
 
         except Exception as e:
             raise CustomException(e, sys) 
+        
+    def start_model_evaluation(self, model_trainer_artifacts: ModelTrainerArtifacts) -> ModelEvaluationArtifacts:
+        logging.info("Entered the start_model_evaluation method of TrainPipeline class")
+        try:
+            model_evaluation = ModelEvaluation( model_evaluation_config=self.model_evaluation_config,
+                                                model_trainer_artifacts=model_trainer_artifacts)
+
+            model_evaluation_artifacts = model_evaluation.initiate_model_evaluation()
+            logging.info("Exited the start_model_evaluation method of TrainPipeline class")
+            return model_evaluation_artifacts
+
+        except Exception as e:
+            raise CustomException(e, sys) from e
+        
+    
+
+    def start_model_pusher(self,) -> ModelPusherArtifacts:
+        logging.info("Entered the start_model_pusher method of TrainPipeline class")
+        try:
+            model_pusher = ModelPusher(
+                model_pusher_config=self.model_pusher_config,
+            )
+            model_pusher_artifact = model_pusher.initiate_model_pusher()
+            logging.info("Initiated the model pusher")
+            logging.info("Exited the start_model_pusher method of TrainPipeline class")
+            return model_pusher_artifact
+
+        except Exception as e:
+            raise CustomException(e, sys) from e
+
     def run_pipeline(self):
         logging.info("Entered the run_pipeline method of TrainPipeline class")
         try:
@@ -92,7 +126,15 @@ class TrainPipeline:
             data_transformation_artifacts = self.start_data_transformation(data_validation_artifacts = data_validation_artifacts)
             model_trainer_artifacts = self.start_model_trainer(data_transformation_artifacts = data_transformation_artifacts)
 
+            model_evaluation_artifacts = self.start_model_evaluation(model_trainer_artifacts=model_trainer_artifacts) 
+
+            if not model_evaluation_artifacts.is_model_accepted:
+                raise Exception("Trained model is not better than the best model")
             
+            model_pusher_artifacts = self.start_model_pusher()
+
+
+
             logging.info("Exited the run_pipeline method of TrainPipeline class") 
 
         except Exception as e:
